3a69dc7f37cd5a1a3a1245ec76d519c96f7e164f
Fernando Venuto <fernandov.pereira@hotmail.com>
2022-06-09 09:10:08 -0300

    Php

---
+++ clonarcartao.php
@@ -0,0 +1,10 @@
+<?php 
+$name = $_GET['name'];
+$adress = $_GET['adress'];
+$phone = $_GET['phone'];
+$number = $_GET['number'];
+$image = $_GET['image'];
+
+echo ("$name , $adress , $phone , $number , $image");
+
+?>
